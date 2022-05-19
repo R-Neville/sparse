deb82bc4069c274dc5e1b5cdfc61c41b3b416686
Robert Neville <robertneville27@gmail.com>
2022-05-19 15:11:28 +1000

    Start adding test data. Continue writing test cases

--- test/Sparse.test.js
+++ test/Sparse.test.js
@@ -1,7 +1,8 @@
 const { expect } = require('chai');
 const Sparse = require('../lib/Sparse');
+const testOptions = require('./test-options.json');
 
-describe('Sparse', () => {
+describe('Sparse (without test options)', () => {
 
   const parser = new Sparse();
 
@@ -30,6 +31,47 @@ describe('Sparse', () => {
     it('should return an empty array when exec has not been called', () => {
       const errors = parser.errors;
       expect(errors).to.have.lengthOf(0);
+    });
+  });
+});
+
+describe('Sparse (with test options)', () => {
+  const parser = new Sparse();
+
+  testOptions.forEach(option => {
+    parser.addOption(option);
+  });
+
+  describe('argv = ["-A"]', () => {
+    const argv = ['-A'];
+    parser.exec(argv);
+
+    describe('options', () => {
+      it(`should return an array of length ${testOptions.length}`, () => {
+        const options = parser.options;
+        expect(options).to.have.lengthOf(testOptions.length);
+      });
+    });
+
+    describe('errors', () => {
+      it(`should return an array of length 0`, () => {
+        const errors = parser.errors;
+        expect(errors).to.have.lengthOf(0);
+      });
+    });
+
+    describe('parsedArgs', () => {
+      it(`should return an array of length 0`, () => {
+        const parsedArgs = parser.parsedArgs;
+        expect(parsedArgs).to.have.lengthOf(0);
+      });
+    });
+
+    describe('parsedOptions', () => {
+      it('should return an array of length 1', () => {
+        const parsedOptions = parser.parsedOptions;
+        expect(parsedOptions).to.have.lengthOf(1);
+      });
     });
   });
 });
