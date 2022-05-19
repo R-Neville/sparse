39a78ea3f63ddf08ebcf1e6cae2171d4033563fa
Robert Neville <robertneville27@gmail.com>
2022-05-19 20:35:55 +1000

    Add suite to test when insufficient arguments are provided for an option

--- test/Sparsely.test.js
+++ test/Sparsely.test.js
@@ -442,4 +442,44 @@ describe('Sparse (with test options)', () => {
       });
     });
   });
+
+  // This suite tests to ensure that
+  // a verbose option is parsed correctly
+  // when insufficient arguments are 
+  // provided to it (appropriate error
+  // messages are generated):  
+  describe('argv = ["--option-B"]', () => {
+    const parser = new Sparsely();
+
+    testOptions.forEach(option => {
+      parser.addOption(option);
+    });
+
+    const argv = [ '--option-B' ];
+    parser.exec(argv);
+
+    describe('errors', () => {
+      const expected = 1;
+      it(`should return an array of length ${expected}`, () => {
+        const errors = parser.errors;
+        expect(errors).to.have.lengthOf(expected);
+      });
+    });
+
+    describe('parsedArgs', () => {
+      const expected = 0;
+      it(`should return an array of length ${expected}`, () => {
+        const parsedArgs = parser.parsedArgs;
+        expect(parsedArgs).to.have.lengthOf(expected);
+      });
+    });
+
+    describe('parsedOptions', () => {
+      const expected = 0;
+      it(`should return an array of length ${expected}`, () => {
+        const parsedOptions = parser.parsedOptions;
+        expect(parsedOptions).to.have.lengthOf(expected);
+      });
+    });
+  });
 }); 
