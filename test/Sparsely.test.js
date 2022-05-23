5a6beb0d42d6afb9a538519c1ff565361a0c4d52
Robert Neville <robertneville27@gmail.com>
2022-05-23 10:39:42 +1000

    Add method to check if an option has been parsed

--- test/Sparsely.test.js
+++ test/Sparsely.test.js
@@ -522,4 +522,43 @@ describe('Sparse (with test options)', () => {
       });
     });
   });
+
+  // This suite tests to ensure that
+  // the isParsedOption method indicates
+  // whether an option has been parsed
+  // or not:
+  describe('argv = ["-A -B"]', () => {
+    const parser = new Sparsely();
+
+    testOptions.forEach(option => {
+      parser.addOption(option);
+    });
+
+    const argv = [ '-A', '-B' ];
+    parser.exec(argv);
+
+    describe('errors', () => {
+      const expected = 1;
+      it(`should return an array of length ${expected}`, () => {
+        const errors = parser.errors;
+        expect(errors).to.have.lengthOf(expected);
+      });
+    });
+
+    describe('parsedArgs', () => {
+      const expected = 0;
+      it(`should return an array of length ${expected}`, () => {
+        const parsedArgs = parser.parsedArgs;
+        expect(parsedArgs).to.have.lengthOf(expected);
+      });
+    });
+
+    describe('parsedOptions', () => {
+      const expected = 1;
+      it(`should return an array of length ${expected}`, () => {
+        const parsedOptions = parser.parsedOptions;
+        expect(parsedOptions).to.have.lengthOf(expected);
+      });
+    });
+  });
 }); 
